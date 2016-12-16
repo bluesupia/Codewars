3a0e691f14314c260d4adc1285c90fa24f6b86a7
kylie.do <kylie.do@daumkakao.com>
2016-12-16 17:39:00 +0900

    Create Phone Number https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java - format modified

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 /**
  * Created by supersupa on 2016. 12. 16..
  * https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java
@@ -6,16 +8,6 @@
 public class Kata {
     public static String createPhoneNumber(int[] numbers) {
         if (numbers.length != 10) return "";
-        return String.format("(%d%d%d) %d%d%d-%d%d%d%d"
-            , numbers[0]
-            , numbers[1]
-            , numbers[2]
-            , numbers[3]
-            , numbers[4]
-            , numbers[5]
-            , numbers[6]
-            , numbers[7]
-            , numbers[8]
-            , numbers[9]);
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
     }
 }
