d6e978dcfd53a80ce998566df56f2e2775b17065
kylie.do <kylie.do@daumkakao.com>
2016-12-29 18:15:12 +0900

    Dead Ants https://www.codewars.com/kata/57d5e850bfcdc545870000b7/solutions/java - best pratice fork - https://www.codewars.com/kata/reviews/57d60aca7764874e6f000042/groups/57d6c4b0a654546222000115

--- src/main/java/Dinglemouse.java
+++ src/main/java/Dinglemouse.java
@@ -8,35 +8,13 @@ import java.util.Arrays;
  */
 public class Dinglemouse {
     public static int deadAntCount(final String ants) {
-
         if (ants == null) return 0;
-        else {
-            String _ants = ants.replaceAll("ant", "").trim();
-            if (_ants.length() == 0) return 0;
-            else {
-                int a_count = (int)Arrays
-                    .stream(_ants.split(""))
-                    .filter(x -> "a".equals(x))
-                    .count();
 
-                int n_count = (int)Arrays
-                    .stream(_ants.split(""))
-                    .filter(x -> "n".equals(x))
-                    .count();
-
-                int t_count = (int)Arrays
-                    .stream(_ants.split(""))
-                    .filter(x -> "t".equals(x))
-                    .count();
-
-                if (a_count >= n_count && a_count >= t_count) {
-                    return a_count;
-                } else if (n_count >= t_count && n_count >= a_count) {
-                    return n_count;
-                } else {
-                    return t_count;
-                }
-            }
-        }
+        String bits = ants.replace("ant", "");
+        return "ant"
+            .chars()
+            .map(x -> (int)bits.chars().filter(y -> x == y).count())
+            .max()
+            .getAsInt();
     }
 }
