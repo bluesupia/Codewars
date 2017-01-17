03caec507cc8ec24b0fb8312eca94a53efd1af49
kylie.do <kylie.do@daumkakao.com>
2017-01-17 15:11:37 +0900

    Histogram - V1  https://www.codewars.com/kata/57c6c2e1f8392d982a0000f2/train/java

--- src/main/java/Dinglemouse.java
+++ src/main/java/Dinglemouse.java
@@ -1,20 +1,54 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
- * Created by supersupa on 2016. 12. 29..
- * Dead Ants
- * https://www.codewars.com/kata/57d5e850bfcdc545870000b7/train/java
- * 6kyu
+ * Created by supersupa on 2017. 1. 17..
+ *
+ * 5kyu / Histogram - V1
+ * https://www.codewars.com/kata/57c6c2e1f8392d982a0000f2/train/java
  */
 public class Dinglemouse {
-    public static int deadAntCount(final String ants) {
-        if (ants == null) return 0;
+    public static String histogram(final int results[]) {
+        int max = Arrays
+                .stream(results)
+                .max()
+                .getAsInt();
 
-        String bits = ants.replace("ant", "");
-        return "ant"
-            .chars()
-            .map(x -> (int)bits.chars().filter(y -> x == y).count())
-            .max()
-            .getAsInt();
+        StringBuffer sb = new StringBuffer();
+        String str = "";
+        for (int i = max; i >= 0; i--) {
+            final int finalI = i;
+            str = Arrays
+                .stream(results)
+                .mapToObj(e -> {
+                    if (e > finalI) {
+                        return "# ";
+                    } else if (e == finalI && e != 0) {
+                        return String.format("%-2s", Integer.toString(e));
+                    } else {
+                        return "  ";
+                    }
+                })
+                .collect(Collectors.joining(""))
+                .replaceAll("\\s+$","");
+            if (str.length() > 0) sb.append(str + "\n");
+        }
+
+        String bar = IntStream
+                .range(0, results.length)
+                .mapToObj(i -> "--")
+                .collect(Collectors.joining(""));
+
+        String end = IntStream
+                .range(1, results.length + 1)
+                .mapToObj(i -> Integer.toString(i))
+                .collect(Collectors.joining(" "));
+
+        sb.append(bar + "\n");
+        sb.append(end + "\n");
+
+        return sb.toString();
     }
+
 }
--- src/test/java/DinglemouseTest.java
+++ src/test/java/DinglemouseTest.java
@@ -1,16 +1,37 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 /**
- * Created by supersupa on 2016. 12. 29..
+ * Created by supersupa on 2017. 1. 17..
  */
 public class DinglemouseTest {
+    @Test
+    public void basic() {
+        final String expected =
+                "    10\n"+
+                "    #\n"+
+                "    #\n"+
+                "7   #\n"+
+                "#   #\n"+
+                "#   #     5\n"+
+                "#   #     #\n"+
+                "# 3 #     #\n"+
+                "# # #     #\n"+
+                "# # # 1   #\n"+
+                "# # # #   #\n"+
+                "-----------\n"+
+                "1 2 3 4 5 6\n";
+        assertEquals(expected, Dinglemouse.histogram(new int[]{7,3,10,1,0,5}));
+    }
 
     @Test
-    public void example() {
-        assertEquals(0, Dinglemouse.deadAntCount("ant ant ant ant"));
-        assertEquals(0, Dinglemouse.deadAntCount(null));
-        assertEquals(2, Dinglemouse.deadAntCount("ant anantt aantnt"));
-        assertEquals(1, Dinglemouse.deadAntCount("ant ant .... a nt"));
+    public void test() {
+        String result = Dinglemouse.histogram(new int[]{0, 0, 0, 0, 0, 0});
+        System.out.println(result);
+
+
+//        String result = Dinglemouse.histogram(new int[]{9, 5, 11, 13, 4, 8});
+//        System.out.println(result);
     }
 }
