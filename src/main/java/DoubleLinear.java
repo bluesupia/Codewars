9120578102837f0ad5d4e174a8f4fe93e9727204
kylie.do <kylie.do@daumkakao.com>
2017-02-09 14:26:56 +0900

    Twice linear - https://www.codewars.com/kata/5672682212c8ecf83e000050/train/java - ** Fixed Tests dblLinear - Timeout: 10000 msecs

---
+++ src/main/java/DoubleLinear.java
@@ -0,0 +1,25 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Created by supersupa on 2017. 2. 8..
+ *
+ * 4kyu / Twice linear
+ * https://www.codewars.com/kata/5672682212c8ecf83e000050/train/java
+ *
+ */
+public class DoubleLinear {
+    public static int dblLinear(int n) {
+
+        SortedSet<Integer> result = new TreeSet<Integer>();
+        result.add(1);
+        int j = 0;
+        for (int i = 0;  i < n; i++) {
+            j = (Integer)result.toArray()[i];
+            result.add( 2 * j + 1 );
+            result.add( 3 * j + 1 );
+        }
+
+        return (Integer)result.toArray()[n];
+    }
+}
