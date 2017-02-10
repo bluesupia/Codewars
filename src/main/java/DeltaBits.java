fec0132a19181364f3e0632b4d832f3f4042008b
kylie.do <kylie.do@daumkakao.com>
2017-02-10 18:20:36 +0900

    Delta bits - https://www.codewars.com/kata/538948d4daea7dc4d200023f/solutions/java

--- src/main/java/DeltaBits.java
+++ src/main/java/DeltaBits.java
@@ -1,16 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
  * Created by supersupa on 2016. 12. 23..
- * Delta Bits
+ *
+ * 5kyu / Delta Bits
  * https://www.codewars.com/kata/538948d4daea7dc4d200023f/train/java
- * 5kyu
  */
 public class DeltaBits {
     public static int convertBits(int a, int b) {
-        String _a = Integer.toBinaryString(a);
-        String _b = Integer.toBinaryString(b);
-        System.out.println(_a);
-        System.out.println(_b);
-        return 0;
+        Pattern p = Pattern.compile("(1)");
+        Matcher m = p.matcher(Integer.toBinaryString(a ^ b));
+        int cnt = 0;
+        while(m.find()) {
+            cnt++;
+        }
+        return cnt;
     }
 }
 
--- src/test/java/DeltaBitsTest.java
+++ src/test/java/DeltaBitsTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -10,6 +11,8 @@ public class DeltaBitsTest {
 
     @Test
     public void test() throws Exception {
-        assertThat(DeltaBits.convertBits(31, 14), is(2));
+//        assertThat(DeltaBits.convertBits(31, 14), is(2));
+        assertThat(DeltaBits.convertBits(7, 17), is(3));
+//        assertThat(DeltaBits.convertBits(312312312, 5645657), is(8));;
     }
 }
